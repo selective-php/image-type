5cb2349aec64d975058a6f583542887140f1f097
peter279k <peter279k@gmail.com>
2019-07-08 14:48:56 +0800

    Resolves issue #34

---
+++ src/Detector/PbmDetector.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Selective\ImageType\Detector;
+
+use Selective\ImageType\ImageType;
+use SplFileObject;
+
+/**
+ * Detector.
+ */
+final class PbmDetector implements DetectorInterface
+{
+    /**
+     * PBM identification.
+     *
+     * @param SplFileObject $file The image file
+     *
+     * @return ImageType|null The image type
+     */
+    public function detect(SplFileObject $file): ?ImageType
+    {
+        $bytes = (string)$file->fread(2);
+
+        return $bytes === "P1" || $bytes === "P4" ? new ImageType(ImageType::PBM) : null;
+    }
+}
